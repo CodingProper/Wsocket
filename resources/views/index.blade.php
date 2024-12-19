c6429ad76244b26885b61694abf78ae2dec15535
CodingProper <muhamedaliwf@mail.ru>
2024-12-19 19:59:59 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="m-auto w-4/5 py-24">
+        <div class="text-center">
+            <h1 class="text-5xl uppercase bold">
+                cars
+            </h1>
+        </div>
+        <div class="m-auto w-4/5 py-24">
+        <div class="text-center">
+            <span>
+                faunded 2020
+            </span>
+        </div>
+    </div>
+@endsection
